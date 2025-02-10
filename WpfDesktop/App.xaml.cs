c6e3cd517ca6da9066e1369dbebf39a61abac13b
Matthias Van Landuyt <matthias@pacar.be>
2025-02-10 09:19:17 +0100

    Prevent app from crashing

--- WpfDesktop/App.xaml.cs
+++ WpfDesktop/App.xaml.cs
@@ -106,6 +106,10 @@ namespace WpfDesktop
         {
             // TODO: Please log and handle the exception as appropriate to your scenario
             // For more info see https://docs.microsoft.com/dotnet/api/system.windows.application.dispatcherunhandledexception?view=netcore-3.0
+
+
+            e.Handled = true;
+            MessageBox.Show(e.Exception.Message, $"An unhandled exception just occurred:\n\n{e.Exception.Message}\n{e.Exception.StackTrace}", MessageBoxButton.OK, MessageBoxImage.Error);
         }
 
     }
